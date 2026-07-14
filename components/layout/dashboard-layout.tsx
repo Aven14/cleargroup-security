86a0281a5583c7c9cf855cc25d6348e50d3f49b1
Aven14 <aven14a@gmail.com>
2026-07-14 23:11:54 +0200

    Fix build errors

--- components/layout/dashboard-layout.tsx
+++ components/layout/dashboard-layout.tsx
@@ -4,7 +4,7 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen">
       <Sidebar />
-      <main className="ml-72 p-8">
+      <main className="ml-64 p-8">
         <div className="max-w-6xl">{children}</div>
       </main>
     </div>
